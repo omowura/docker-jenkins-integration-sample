01e64aba1da6af05953e89878e2572a94ec6d208
Sulaimon Ekundayoe <omowura4u@gmaile.com>
2022-09-09 22:07:27 +0300

    Create Jarfile

--- src/main/java/TestDemo.java
+++ src/main/java/TestDemo.java
@@ -1,6 +1,11 @@
 public class TestDemo {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("CONTINUOUS INTEGRATION & CONTINUOUS DEPLOYMENT/RELEASE");
+        System.out.println("Continuous integration and deployment in action");
+        System.out.println("Hello there! It's been amazing in this QA journey. I started this journey about 4 months ago. I have no\n" +
+                "    regret of whatsoever learning more of it on daily basis.");
+        System.out.println("I'm sure you are enjoying it as well! ");
 
 
     }
